f3ba575ceb90227dc974be3e1e549cecc1e33554
Doug Orleans <dougorleans@gmail.com>
2012-03-25 22:23:13 -0400

    Build prototype objects directly.

--- wordsearch.js
+++ wordsearch.js
@@ -41,19 +41,21 @@ function Game() {
 
   this.board.placeTiles(this.tiles);
 }
+Game.prototype = {
 
-Game.prototype.makeAllTiles = function (tileData) {
-  this.tiles = [];
-  var game = this;
-  $.each(tileData, function (_, args) {
-    game.makeTiles.apply(game, args);
-  });
-  $.shuffle(this.tiles);
-}
+  makeAllTiles: function (tileData) {
+    this.tiles = [];
+    var game = this;
+    $.each(tileData, function (_, args) {
+      game.makeTiles.apply(game, args);
+    });
+    $.shuffle(this.tiles);
+  },
 
-Game.prototype.makeTiles = function (letter, value, freq) {
-  for (var i = 0; i < freq; i++) {
-    this.tiles.push(new Tile(this, letter, value));
+  makeTiles: function (letter, value, freq) {
+    for (var i = 0; i < freq; i++) {
+      this.tiles.push(new Tile(this, letter, value));
+    }
   }
 }
 
@@ -61,51 +63,53 @@ function Board(game) {
   this.makeView(game.view);
   this.makeSpaces();
 }
+Board.prototype = {
 
-Board.prototype.makeSpaces = function () {
-  this.spaces = [];
-  for (var r = 0; r < boardSize; r++) {
-    this.spaces.push([]);
-    for (var c = 0; c < boardSize; c++) {
-      this.makeSpace(r, c);
-    }
-  }
-}
-
-Board.prototype.makeSpace = function (r, c) {
-  var space = new Space(this, r, c);
-  this.spaces[r][c] = space;
-}
-
-Board.prototype.spaceAt = function (r, c) {
-  return this.spaces[r][c];  
-}
-
-Board.prototype.placeTiles = function (tiles) {
-  for (var r = 0; r < boardSize; r++) {
-    for (var c = 0; c < boardSize; c++) {
-      if (r < boardSize/2 - 1 || r > boardSize/2 ||
-          c < boardSize/2 - 1 || c > boardSize/2) {
-        this.placeTile(tiles.pop(), r, c);
+  makeSpaces: function () {
+    this.spaces = [];
+    for (var r = 0; r < boardSize; r++) {
+      this.spaces.push([]);
+      for (var c = 0; c < boardSize; c++) {
+        this.makeSpace(r, c);
       }
     }
+  },
+
+  makeSpace: function (r, c) {
+    var space = new Space(this, r, c);
+    this.spaces[r][c] = space;
+  },
+
+  spaceAt: function (r, c) {
+    return this.spaces[r][c];  
+  },
+
+  placeTiles: function (tiles) {
+    for (var r = 0; r < boardSize; r++) {
+      for (var c = 0; c < boardSize; c++) {
+        if (r < boardSize/2 - 1 || r > boardSize/2 ||
+            c < boardSize/2 - 1 || c > boardSize/2) {
+          this.placeTile(tiles.pop(), r, c);
+        }
+      }
+    }
+  },
+
+  placeTile: function (tile, r, c) {
+    var space = this.spaceAt(r, c);
+    space.placeTile(tile);
+    tile.board = this;
+    tile.origin = space;
+  },
+
+  makeView: function (parent) {
+    var view = $('<div id="board" />').appendTo(parent);
+    view.data('model', this);
+    this.view = board;
+
+    view.width(tileRadius*boardSize*3);
+    view.height(tileRadius*boardSize*3);
   }
-}
-
-Board.prototype.placeTile = function (tile, r, c) {
-  var space = this.spaceAt(r, c);
-  space.placeTile(tile);
-  tile.board = this;
-  tile.origin = space;
-}  
-
-Board.prototype.makeView = function (parent) {
-  var view = $('<div id="board" />').appendTo(parent);
-  view.data('model', this);
-  this.view = board;
-
-  view.width(tileRadius*boardSize*3);
-  view.height(tileRadius*boardSize*3);
 }
 
 function Space(board, r, c) {
@@ -113,19 +117,31 @@ function Space(board, r, c) {
   this.c = c;
   this.makeView(board.view);
 }
+Space.prototype = {
 
-Space.prototype.makeView = function (parent) {
-  var view = $('<canvas class="space" />').appendTo(parent);
-  view.data('model', this);
-  this.view = view;
+  makeView: function (parent) {
+    var view = $('<canvas class="space" />').appendTo(parent);
+    view.data('model', this);
+    this.view = view;
 
-  view.attr('width', tileRadius*3);
-  view.attr('height', tileRadius*3);
-  drawCircle(view, { x: tileRadius*1.5, y: tileRadius*1.5 });
+    view.attr('width', tileRadius*3);
+    view.attr('height', tileRadius*3);
+    drawCircle(view, { x: tileRadius*1.5, y: tileRadius*1.5 });
 
-  view.position({ my: 'left top', at: 'left top', of: parent,
-                  collision: 'none',
-                  offset: this.c*tileRadius*3 + ' ' + this.r*tileRadius*3 });
+    view.position({ my: 'left top', at: 'left top', of: parent,
+                    collision: 'none',
+                    offset: this.c*tileRadius*3 + ' ' + this.r*tileRadius*3 });
+  },
+
+  placeTile: function (tile) {
+    var oldSpace = tile.space;
+    if (oldSpace) oldSpace.tile = null;
+    this.tile = tile;
+    tile.space = this;
+
+    tile.view.position({ my: 'left top', at: 'left top', of: this.view,
+                         collision: 'none' });
+  }
 }
 
 function drawCircle(canvas, p) {
@@ -135,115 +151,107 @@ function drawCircle(canvas, p) {
   canvas.drawEllipse(p);
 }
 
-Space.prototype.placeTile = function (tile) {
-  var oldSpace = tile.space;
-  if (oldSpace) oldSpace.tile = null;
-  this.tile = tile;
-  tile.space = this;
-
-  tile.view.position({ my: 'left top', at: 'left top', of: this.view,
-                       collision: 'none' });
-}
-
 function Tile(game, letter, value) {
   this.letter = letter;
   this.value = value;
   this.makeView(game.view);
 }
+Tile.prototype = {
 
-Tile.prototype.legalSpaces = function() {
-  var tile = this;
-  var board = this.board;
-  var r0 = this.origin.r;
-  var c0 = this.origin.c;
-  var spaces = [];
-  if (!this.origin.tile) spaces.push(this.origin);
-  $.each([-1, 0, 1], function (_, Δr) {
-    $.each([-1, 0, 1], function (_, Δc) {
-      if (Δr || Δc) {
-        for (var i = 1; i < boardSize; i++) {
-          var r = r0 + i*Δr;
-          var c = c0 + i*Δc;
-          if (r < 0 || r >= boardSize || c < 0 || c >= boardSize) break;
-          var space = board.spaceAt(r, c);
-          var tileInSpace = space.tile;
-          if (tileInSpace && tileInSpace != tile) break;
-          spaces.push(space);
+  legalSpaces: function() {
+    var tile = this;
+    var board = this.board;
+    var r0 = this.origin.r;
+    var c0 = this.origin.c;
+    var spaces = [];
+    if (!this.origin.tile) spaces.push(this.origin);
+    $.each([-1, 0, 1], function (_, Δr) {
+      $.each([-1, 0, 1], function (_, Δc) {
+        if (Δr || Δc) {
+          for (var i = 1; i < boardSize; i++) {
+            var r = r0 + i*Δr;
+            var c = c0 + i*Δc;
+            if (r < 0 || r >= boardSize || c < 0 || c >= boardSize) break;
+            var space = board.spaceAt(r, c);
+            var tileInSpace = space.tile;
+            if (tileInSpace && tileInSpace != tile) break;
+            spaces.push(space);
+          }
         }
+      });
+    });
+    return spaces;
+  },
+
+  makeView: function (parent) {
+    var view = $('<div class="tile" />').appendTo(parent);
+    view.data('model', this);
+    this.view = view;
+
+    view.width(tileRadius*3);
+    view.height(tileRadius*3);
+
+    var canvas = $('<canvas />').appendTo(view);
+    canvas.attr('width', tileRadius*3);
+    canvas.attr('height', tileRadius*3);
+    canvas.addLayer({
+      method: 'drawEllipse',
+      x: tileRadius*1.5,
+      y: tileRadius*1.5,
+      width: tileRadius*2,
+      height: tileRadius*2,
+      strokeStyle: 'black',
+      fillStyle: 'tan'
+    });
+    canvas.drawLayers();
+
+    view.mousedown(function (e) {
+      if (canvas.getLayers().length < 2) {
+        canvas.addLayer({
+          method: 'drawEllipse',
+          strokeStyle: 'red',
+          strokeWidth: 5,
+          x: tileRadius*1.5,
+          y: tileRadius*1.5,
+          width: (tileRadius+3)*2,
+          height: (tileRadius+3)*2
+        });
+      } else {
+        canvas.removeLayer(1);
+      }
+      canvas.drawLayers();
+    });
+
+    var label = $('<div class="label" />').appendTo(view);
+    $('<div class="letter">' + this.letter + '</div>').appendTo(label);
+    if (this.value) {
+      $('<sub class="value">' + this.value + '</sub>').appendTo(label);
+    }
+    label.position({ my: 'center', at: 'center', of: canvas });
+
+    var tile = this;
+    view.draggable({
+      distance: 5,
+      stack: '.tile',
+      revert: function (space) {
+        if (!space) {
+          return true;
+        } else {
+          space.data('model').placeTile(tile);
+          return false;
+        }
+      },
+      revertDuration: 200,
+      start: function (e) {
+        $.each(tile.legalSpaces(), function (_, space) {
+          space.view.droppable();
+        });
+      },
+      stop: function (e) {
+        $('.space').droppable('destroy');
       }
     });
-  });
-  return spaces;
-}
-
-Tile.prototype.makeView = function (parent) {
-  var view = $('<div class="tile" />').appendTo(parent);
-  view.data('model', this);
-  this.view = view;
-
-  view.width(tileRadius*3);
-  view.height(tileRadius*3);
-
-  var canvas = $('<canvas />').appendTo(view);
-  canvas.attr('width', tileRadius*3);
-  canvas.attr('height', tileRadius*3);
-  canvas.addLayer({
-    method: 'drawEllipse',
-    x: tileRadius*1.5,
-    y: tileRadius*1.5,
-    width: tileRadius*2,
-    height: tileRadius*2,
-    strokeStyle: 'black',
-    fillStyle: 'tan'
-  });
-  canvas.drawLayers();
-
-  view.mousedown(function (e) {
-    if (canvas.getLayers().length < 2) {
-      canvas.addLayer({
-        method: 'drawEllipse',
-        strokeStyle: 'red',
-        strokeWidth: 5,
-        x: tileRadius*1.5,
-        y: tileRadius*1.5,
-        width: (tileRadius+3)*2,
-        height: (tileRadius+3)*2
-      });
-    } else {
-      canvas.removeLayer(1);
-    }
-    canvas.drawLayers();
-  });
-
-  var label = $('<div class="label" />').appendTo(view);
-  $('<div class="letter">' + this.letter + '</div>').appendTo(label);
-  if (this.value) {
-    $('<sub class="value">' + this.value + '</sub>').appendTo(label);
   }
-  label.position({ my: 'center', at: 'center', of: canvas });
-
-  var tile = this;
-  view.draggable({
-    distance: 5,
-    stack: '.tile',
-    revert: function (space) {
-      if (!space) {
-        return true;
-      } else {
-        space.data('model').placeTile(tile);
-        return false;
-      }
-    },
-    revertDuration: 200,
-    start: function (e) {
-      $.each(tile.legalSpaces(), function (_, space) {
-        space.view.droppable();
-      });
-    },
-    stop: function (e) {
-      $('.space').droppable('destroy');
-    }
-  });
 }
 
 var game;
